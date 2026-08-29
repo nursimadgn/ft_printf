82264b12279868ad2d7f2a037b659603930f2be1
nursimadgn <nursimadgn34@gmail.com>
2026-08-29 18:49:36 +0300

    the functions were shortened

--- ft_printf.c
+++ ft_printf.c
@@ -6,70 +6,56 @@
 /*   By: seldogan <seldogan@student.42istanbul.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/27 17:38:18 by seldogan          #+#    #+#             */
-/*   Updated: 2026/08/29 18:27:52 by seldogan         ###   ########.fr       */
+/*   Updated: 2026/08/29 18:48:14 by seldogan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-void ft_findformat(char format, va_list *args)
+void	ft_findformat(char format, va_list *args)
 {
-    if(format == 'd')
-    {
-        int res_d;
-        res_d = (int)va_arg(*args, int);
-        ft_putnbr(res_d);   
-    }
-    if(format == 'c')
-    {
-        char res_c;
-        res_c = (char)va_arg(*args, int);
-        write(1, &res_c, 1);
-    }
-    if(format == 's')
-    {
-        char *res_s;
-        res_s = va_arg(*args, char *);
-        ft_putstr(res_s);
-    }
+	if (format == 'd' || format == 'i')
+		ft_putnbr(va_arg(*args, int));
+	if (format == 'c')
+		ft_putchar((va_arg(*args, int)));
+	if (format == 's')
+		ft_putstr(va_arg(*args, char *));
 }
 
-int ft_printf(const char *first, ...)
+int	ft_printf(const char *first, ...)
 {
-    int i;
-    int len;
-    len = 0;
-    i = 0;
-    
-    va_list args;
-    
-    va_start(args, first);
+	int		i;
+	int		len;
+	va_list	args;
 
-    while(first[i] != '\0')
-    {
-        if(first[i] == '%')
-        {
-            i++;
-            if(first[i] != '\0')
-            {
-                ft_findformat(first[i], &args);
-            }
-        }
-        else
-        {
-            len += write(1, &first[i], 1);
-        }
-        i++; 
-    }
-    return(len);
+	len = 0;
+	i = 0;
+	va_start(args, first);
+	while (first[i] != '\0')
+	{
+		if (first[i] == '%')
+		{
+			i++;
+			if (first[i] != '\0')
+			{
+				ft_findformat(first[i], &args);
+			}
+		}
+		else
+		{
+			len += write(1, &first[i], 1);
+		}
+		i++;
+	}
+	return (len);
 }
 
 #include <stdio.h>
 
-int main()
+int	main(void)
 {
-    char a = 'a';
-    int  b = 5;
-    char s[] = "hello";     
-    ft_printf("d: %d, c: %c, s: %s", b, a, s);
+	char a = 'a';
+	int b = 5;
+	char s[] = "hello";
+	ft_printf("d: %d, c: %c, s: %s", 0x10, a, s);
 }
